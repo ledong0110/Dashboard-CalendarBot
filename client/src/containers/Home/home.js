1e9021e95dc16ffa1c5957de1bfe402ce34263f9
SonTheeTheson2002SonThee <theson2604@gmail.com>
2023-04-15 18:06:46 +0700

    editing home page & setting page

--- client/src/containers/Home/home.js
+++ client/src/containers/Home/home.js
@@ -4,14 +4,24 @@ import React, { Component } from 'react'
 import { Redirect } from "react-router-dom";
 import { withStyles } from "@material-ui/core/styles";
 import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 
 
-const useStyles = theme => ({
-    root: {
-
-    }
-
-})
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
 
 
 class Home extends Component {
@@ -30,7 +40,7 @@ class Home extends Component {
 
 
     render() {
-        const { classes } = this.props;
+        const classes = useStyles();
         const isAuth = this.handleAuth();
 
         if (!isAuth) {
@@ -39,11 +49,11 @@ class Home extends Component {
         return (
             <div className={classes.root}>
 
-                <img src="/bachkhoa.png" width={500} height={500}/>
-                <Button variant="contained" href="/signup" color="primary">
+                <img className={classes.avatar} src="/bachkhoa.png" width={500} height={500}/>
+                <Button className={classes.submit} variant="contained" href="/signup" color="primary">
                     Sign Up
                 </Button>
-                <Button variant="contained" href="/login" color="secondary">
+                <Button className={classes.submit} variant="contained" href="/login" color="secondary">
                     Login
                 </Button>
             </div>
